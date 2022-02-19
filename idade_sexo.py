7905e10dd5acb8453e9409abe1162be37635a7f2
Vitória Pimentel <97207959+vitpimentell@users.noreply.github.com>
2022-02-19 15:58:55 -0300

    Create idade_sexo.py

---
+++ idade_sexo.py
@@ -0,0 +1,24 @@
+''' Crie um programa que leia a idade e o sexo de várias pessoas. A cada pessoa cadastrada, o programa devrá perguntar se o usuário quer ou não continuar. No final, mostre:
+
+A) quantas pessoas tem mais de 18 anos
+B) quantos homens foram cadastrados
+C) quantas mulheres com menos de 20 anos foram cadastradas'''
+
+soma = cont_idade = cont_homens = cont_mulheres = 0
+while True:
+    i = int(input('Idade: '))
+    if i >= 18:
+        cont_idade += 1
+    s = str(input('Sexo [F/M]: ')).upper().strip()
+    while s != 'F' and s != 'M':
+        s = str(input('Sexo [F/M]: ')).upper().strip()
+    if s == 'M':
+        cont_homens += 1
+    if s == 'F' and i < 20:
+        cont_mulheres += 1
+    p = str(input('Quer continuar? [S/N] ')).upper().strip()
+    while p != 'S' and p != 'N':
+        p = str(input('Quer continuar? [S/N] ')).upper().strip()
+    if p == 'N':
+        break
+print(f'Fim do programa. Foram cadastradas {cont_idade} pessoas. Destas, {cont_homens} eram homens e {cont_mulheres} eram mulheres com menos de 20 anos')
